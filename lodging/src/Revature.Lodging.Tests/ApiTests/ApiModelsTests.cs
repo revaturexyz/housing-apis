fa2d3e6596021a313a74905dab2d03dda291c3b0
Rodney Canlas <canlasrj@gmail.com>
2020-02-27 11:13:50 -0600

    Created test project structure.

---
+++ lodging/src/Revature.Lodging.Tests/ApiTests/ApiModelsTests.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Revature.Lodging.Tests.ApiTests
+{
+   public class ApiModelsTests
+  {
+  }
+}
---
+++ lodging/src/Revature.Lodging.Tests/ApiTests/ComplexControllerTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Revature.Lodging.Tests.ApiTests
+{
+    class ComplexController
+    {
+    }
+}
