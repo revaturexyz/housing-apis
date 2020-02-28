192669637927567eca18b809f30b228fa8047366
Steven Alvarez <steven.alvarez.a@gmail.com>
2020-02-28 10:23:04 -0600

    added TenentAccount entity and columns

---
+++ identity/src/Revature.Identity.DataAccess/Entities/TenantAccount.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Revature.Identity.DataAccess.Entities
+{
+  public class TenantAccount
+  {
+    public Guid TenantId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+  }
+}
