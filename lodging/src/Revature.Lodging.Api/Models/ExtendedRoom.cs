5c676fcf7c210ef41a7a3bcfe0f6ab676d2b5511
Rodney Canlas <canlasrj@gmail.com>
2020-02-28 11:18:27 -0600

    Created FloorPlan API model.

--- lodging/src/Revature.Lodging.Api/Models/ExtendedRoom.cs
+++ lodging/src/Revature.Lodging.Api/Models/ExtendedRoom.cs
@@ -7,17 +7,23 @@ namespace Revature.Lodging.Api.Models
 {
   public class ExtendedRoom
   {
-    public Guid Id { get; set; }
+    public Guid ComplexId { get; set; }
 
     public string RoomNumber { get; set; }
 
     public int NumberBeds { get; set; }
 
+    public int CurrentOccupants { get; set; }
+
     public Guid FloorPlanId { get; set; }
 
-    public string Gender { get; set; }
+    public int Gender { get; set; }
 
-    public string RoomType { get; set; }
+    public int RoomType { get; set; }
+
+    public DateTime? StartDate { get; set; }
+
+    public DateTime? EndDate { get; set; }
 
     public IEnumerable<Amenity> Amenities { get; set; }
   }
