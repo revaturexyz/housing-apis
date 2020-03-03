58e427c5533dafd8db8565c2b87f0bbbddfac095
Rodney Canlas <canlasrj@gmail.com>
2020-03-02 22:26:53 -0600

    Brought in Revature.Lodging.Lib namespace.

--- lodging/src/Revature.Lodging.Api/Controllers/TenantController.cs
+++ lodging/src/Revature.Lodging.Api/Controllers/TenantController.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Revature.Room.Lib;
+using Revature.Lodging.Lib;
 
 namespace Revature.Room.Api.Controllers
 {
